67021b88e0a419746493a245bebae2ec8ec18e8c
Shree06-patel <52357869+Shree06-patel@users.noreply.github.com>
2020-09-22 16:11:16 +0530

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,8 @@
 module.exports = {
   someSidebar: {
     LocalReference: ['doc1', 'doc2', 'doc3'],
+    APIReference : ['Objects','Methods'],
+    Objects : ['doc6','doc7'],
     Methods: ['doc4','doc5'],
     Features: ['mdx'],
   },
