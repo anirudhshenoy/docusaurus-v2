527cd15e1bf98e3510cc778e5aaa2ce708a37616
Shree06-patel <52357869+Shree06-patel@users.noreply.github.com>
2020-09-23 09:58:13 +0530

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,8 +1,8 @@
 module.exports = {
   someSidebar: {
     //LocalReference: ['doc1', 'doc2', 'doc3'],
-    Methods: ['doc4','doc5'],
-    Objects: ['doc6','doc7'],
+    Objects: ['allFunctions','allSteps'],
+    Methods: ['sendTextMessage','executeFunction'],
     YMReleaseNotes : ['Sept 2020', 'Aug 2020'],
   },
 };
